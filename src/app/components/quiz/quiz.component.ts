9e9485da74ad2d680f5d3e19993e5921038e5b60
alvarobilbao <bilbialvaro@gmail.com>
2018-11-22 14:03:40 -0400

    next question basic functionality

--- src/app/components/quiz/quiz.component.ts
+++ src/app/components/quiz/quiz.component.ts
@@ -10,15 +10,35 @@ import { Component, OnInit } from '@angular/core';
 export class QuizComponent implements OnInit {
 
   questions: Question[];
+  currentQuestion: Question;
+  currentQuestionId: number;
+  currentQuestionIndex: number;
+  answer: any;
   constructor(private questionService: JsonquestionService) { }
 
   ngOnInit() {
     this.questionService.getQuestions().subscribe(data => {
       console.log(data);
       this.questions = data;
+      this.currentQuestion = this.questions[0];
+      this.currentQuestionIndex = 0;
+      this.currentQuestionId = 0;
     });
   }
-  
-  
 
+  nextQuestion() {
+    const questionId = this.currentQuestionId;
+    if (questionId === 4 && this.answer === 'No') {
+      this.currentQuestionId = 12;
+    } else {
+      this.currentQuestionId++;
+    }
+    for (let i = this.currentQuestionIndex; i < this.questions.length; i++) {
+      if (this.questions[i].id === this.currentQuestionId) {
+        this.currentQuestionIndex = i;
+        this.currentQuestion = this.questions[i];
+        break;
+      }
+    }
+  }
 }
